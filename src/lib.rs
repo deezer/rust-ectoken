14d85584bc0a75936057a7d788dbe13cf26c210a
Benoît Burnichon <bburnichon@deezer.com>
2020-01-06 21:28:47 +0100

    Try switch to ring instead of rust-crypto

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,14 @@
 #![deny(missing_docs)]
 //! This module could be used to encrypt/decrypt tokens
 
-use crypto::aead::{AeadDecryptor, AeadEncryptor};
-use crypto::aes;
-use crypto::aes_gcm::AesGcm;
-use crypto::digest::Digest;
-use crypto::sha2::Sha256;
 use rand::RngCore;
+use ring::aead;
+use ring::digest;
 use std::error;
 use std::fmt;
 
 const NONCE_LEN: usize = 12;
 const TAG_LEN: usize = 16;
-const KEY_LEN: usize = 32;
 
 /// Real derived key
 #[derive(Clone, PartialEq)]
@@ -26,54 +22,72 @@ impl Ec3Key {
 
     /// Create a new key from raw string
     pub fn new_raw(key: &[u8]) -> Self {
-        let mut hasher = Sha256::new();
-
-        hasher.input(key);
-
-        let mut key_hash = [0u8; KEY_LEN];
-        hasher.result(&mut key_hash);
-
-        Self(key_hash)
+        let mut digest = [0u8; digest::SHA256_OUTPUT_LEN];
+        digest.clone_from_slice(&digest::digest(&digest::SHA256, key).as_ref());
+        Self(digest)
     }
 
     /// Encrypts token with key
     pub fn encrypt(&self, token: &str) -> String {
-        let mut nonce = [0u8; NONCE_LEN];
+        let mut nonce = [0u8; aead::NONCE_LEN];
         let mut generator = rand::thread_rng();
 
         generator.fill_bytes(&mut nonce);
 
-        let mut crypto = AesGcm::new(aes::KeySize::KeySize256, &self.0, &nonce, &[]);
-        let mut output = vec![0u8; token.len()];
-        let mut tag = [0u8; TAG_LEN];
+        let key = aead::UnboundKey::new(&aead::AES_256_GCM, &self.0)
+            .expect("Key has fixed length, could not fail");
 
-        crypto.encrypt(token.as_bytes(), &mut output, &mut tag);
+        let mut in_out = token.as_bytes().to_vec();
+
+        let less_safe_key = aead::LessSafeKey::new(key);
+        let tag = less_safe_key
+            .seal_in_place_separate_tag(
+                aead::Nonce::assume_unique_for_key(nonce),
+                aead::Aad::from(&[]),
+                &mut in_out,
+            )
+            .expect("should not fail");
 
         let mut encrypted = Vec::with_capacity(NONCE_LEN + token.len() + TAG_LEN);
 
-        encrypted.extend_from_slice(&nonce);
-        encrypted.extend_from_slice(&output);
-        encrypted.extend_from_slice(&tag);
+        encrypted.extend_from_slice(nonce.as_ref());
+        encrypted.extend_from_slice(&in_out);
+        encrypted.extend_from_slice(tag.as_ref());
 
         base64::encode_config(&encrypted, base64::URL_SAFE_NO_PAD)
     }
 
     /// Decrypt given token with already derived key
     pub fn decrypt(&self, token: &str) -> Result<String, DecryptionError> {
-        let chars = base64::decode_config(token, base64::URL_SAFE_NO_PAD)?;
+        let mut chars = base64::decode_config(token, base64::URL_SAFE_NO_PAD)?;
+        let length = chars.len();
 
-        if chars.len() < (NONCE_LEN + TAG_LEN) as usize {
+        if length < (NONCE_LEN + TAG_LEN) as usize {
             return Err(DecryptionError::IOError("invalid input length"));
         }
 
-        let mut crypto = AesGcm::new(aes::KeySize::KeySize256, &self.0, &chars[..NONCE_LEN], &[]);
-        let mut output = vec![0u8; chars.len() - (NONCE_LEN + TAG_LEN)];
+        let less_key = aead::LessSafeKey::new(
+            aead::UnboundKey::new(&aead::AES_256_GCM, &self.0).expect("should not fail"),
+        );
+        let mut nonce = [0u8; 12];
+        nonce.clone_from_slice(&chars[..NONCE_LEN]);
+        let result = less_key.open_in_place(
+            aead::Nonce::assume_unique_for_key(nonce),
+            aead::Aad::from(&[]),
+            &mut chars[NONCE_LEN..],
+        );
 
-        if ! crypto.decrypt(&chars[NONCE_LEN as usize..chars.len() - TAG_LEN], &mut output, &chars[chars.len() - TAG_LEN..]) {
+        if result.is_err() {
             return Err(DecryptionError::IOError("decryption failed"));
         }
 
-        let s = String::from_utf8(output)?;
+        let s = String::from_utf8(
+            chars
+                .into_iter()
+                .skip(NONCE_LEN)
+                .take(length - NONCE_LEN - TAG_LEN)
+                .collect(),
+        )?;
 
         Ok(s)
     }
@@ -127,7 +141,9 @@ impl fmt::Display for DecryptionError {
         match *self {
             DecryptionError::InvalidBase64(_) => write!(f, "Invalid base64."),
             DecryptionError::InvalidUTF8(_) => write!(f, "Invalid UTF8 string decrypted."),
-            DecryptionError::IOError(description) => write!(f, "Input/Output error: {}", description),
+            DecryptionError::IOError(description) => {
+                write!(f, "Input/Output error: {}", description)
+            }
         }
     }
 }
@@ -168,15 +184,24 @@ mod tests {
 
     #[test]
     fn it_returns_err_on_invalid_base64_string() {
-        let decrypted = decrypt_v3("testkey123", "af0c6acf7906cd500aee63a4dd2e97ddcb0142601cf83aa9d622289718c4c85413");
+        let decrypted = decrypt_v3(
+            "testkey123",
+            "af0c6acf7906cd500aee63a4dd2e97ddcb0142601cf83aa9d622289718c4c85413",
+        );
 
-        assert!(decrypted.is_err(), "decryption should be an Error with invalid base64 encoded string");
+        assert!(
+            decrypted.is_err(),
+            "decryption should be an Error with invalid base64 encoded string"
+        );
     }
 
     #[test]
     fn it_returns_err_on_invalid_length() {
         let decrypted = decrypt_v3("testkey123", "bs4W7wyy");
 
-        assert!(decrypted.is_err(), "decryption should be an Error with invalid length encoded string");
+        assert!(
+            decrypted.is_err(),
+            "decryption should be an Error with invalid length encoded string"
+        );
     }
 }
